582107e79d7526cadf32b64a9c909d1c34782a52
Deleema Fernando <DFernando@ieDigital.com>
2019-10-09 08:46:10 +0530

    cs and grpc

--- cs_client/Program.cs
+++ cs_client/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using Grpc.Core;
+using Demo.CalcPb;
+using System.Threading.Tasks;
 
 namespace cs_client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+            var client = new CalculatorService.CalculatorServiceClient(channel);
+
+            var result = await client.AddAsync(new AddRequest
+            {
+                No1 = 1,
+                No2 = 2,
+            });
+
+            Console.WriteLine(result);
+            await channel.ShutdownAsync();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
