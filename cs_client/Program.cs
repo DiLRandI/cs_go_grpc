619e4540bf782efc459e34bba4979c03dafa0e4c
Deleema Fernando <DFernando@ieDigital.com>
2019-10-09 09:09:17 +0530

    Update pb gen location. More than one call to server.

--- cs_client/Program.cs
+++ cs_client/Program.cs
@@ -18,7 +18,22 @@ namespace cs_client
                 No2 = 2,
             });
 
+            var result1 = await client.AddAsync(new AddRequest
+            {
+                No1 = 2,
+                No2 = 5,
+            });
+
+            var result2 = await client.AddAsync(new AddRequest
+            {
+                No1 = 5,
+                No2 = 10,
+            });
+
             Console.WriteLine(result);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
             await channel.ShutdownAsync();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
